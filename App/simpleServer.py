f3ed7785e48e3bb78d1aa305aad2464122cec8ac
Yicong-Huang <hyc541978023@gmail.com>
2019-06-21 14:31:51 -0700

    convert db config path to absolute path

--- App/simpleServer.py
+++ App/simpleServer.py
@@ -1,31 +1,32 @@
-from flask import Flask, request, send_from_directory, make_response
-import psycopg2
+from flask import Flask, send_from_directory, make_response
 
-app = Flask(__name__,static_url_path='')
+app = Flask(__name__, static_url_path='')
 
-#establish remote db connection
-conn = psycopg2.connect(dbname='testdb', user='tester', password='testpassword', host='52.53.237.95', port='5432', sslmode='require')
-cur = conn.cursor()
+from data_preparation.connection import Connection
+
+conn = Connection()()
 
 
 @app.route("/temp")
 def send_temp_data():
-  resp = make_response(send_from_directory('data','temp.csv'))
-  resp.headers['Access-Control-Allow-Origin'] = '*'
-  return resp
+    resp = make_response(send_from_directory('data', 'temp.csv'))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
 
 @app.route("/realtime")
 def send_realTime_data():
-  resp = make_response(send_from_directory('data','realtime.txt'))
-  resp.headers['Access-Control-Allow-Origin'] = '*'
-  return resp
+    resp = make_response(send_from_directory('data', 'realtime.txt'))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
 
 @app.route("/tweets")
 def send_tweets_data():
-  resp = make_response(send_from_directory('data','tweets-wildfire.json'))
-  resp.headers['Access-Control-Allow-Origin'] = '*'
-  return resp
+    resp = make_response(send_from_directory('data', 'tweets-wildfire.json'))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
---
+++ launch.py
@@ -0,0 +1,4 @@
+from App.simpleServer import app
+
+if __name__ == '__main__':
+    app.run()

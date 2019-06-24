8f359327b2c09d585ad2c6afe6907788de0157c4
Hugo Huang <qiaonanh@uci.edu>
2019-06-24 10:58:58 -0700

    Truncate table, image change from set to obj

--- twitterPhotoUrl.py
+++ twitterPhotoUrl.py
@@ -3,40 +3,72 @@ import json
 import re
 import urllib.request
 from bs4 import BeautifulSoup
+from connection import Connection
+import psycopg2
+
+
+def truncate_table(table_name):
+    conn = Connection()()
+    cur = conn.cursor()
+    cur.execute('TRUNCATE TABLE ' + table_name)
+    cur.close()
+    conn.commit()
+
+
 def getTwitterImage(link):
     if requests.get(link).status_code < 400:
         source = urllib.request.urlopen(expandedUrl).read()
-        soup = BeautifulSoup(source,'html.parser')
-        imgs = soup.findAll("div",{"class":"AdaptiveMedia-photoContainer js-adaptive-photo" })
-        imgUrl = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',str(imgs))
-        uniqueImgUrl= list(set(imgUrl))
+        soup = BeautifulSoup(source, 'html.parser')
+        imgs = soup.findAll("div", {"class": "AdaptiveMedia-photoContainer js-adaptive-photo"})
+        imgUrl = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(imgs))
+        uniqueImgUrl = list(set(imgUrl))
         return uniqueImgUrl
     else:
-        #print("404 not found")
+        # print("404 not found")
         return -1
 
-with open("tweets_urls" , 'rb') as file:
+
+truncate_table('images')
+
+with open("tweets_urls", 'rb') as file:
     data = json.load(file)
-    tweetPhoto_dict = dict() #this dictionary stores id: url that contains tweets that have pics
+    tweetPhoto_dict = dict()  # this dictionary stores id: url that contains tweets that have pics
     cnt = 0
+    amt = 0
+    conn = Connection()()
     for id, item in data.items():
-        print(cnt)
-        cnt += 1
+        amt += 1
+        if amt % 500  == 0:
+            print("ID processed: "+ str(amt) + ", containing images (url): " + str(cnt))
         for element in item:
-            expandedUrl = requests.get(element).url
-            #print("expandedUrl is: " + expandedUrl)
-            #in the situation that the url links to a tweet with image
-            if (expandedUrl.find("twitter")!=-1)and(expandedUrl.find("photo")!=-1):
-                #print("the url links to a tweet with photo")
-                photoUrl = getTwitterImage(expandedUrl)
-                if photoUrl != -1:
-                    tweetPhoto_dict.update({id:photoUrl})
+            try:
+                # print("id is: "+str(id))
+                # print("shorten link is "+element)
+                expandedUrl = requests.get(element).url
+                # print("expandedUrl is: " + expandedUrl)
+                # in the situation that the url links to a tweet with image
+                if (expandedUrl.find("twitter") != -1):
+                    # print("the url links to a tweet with photo")
+                    photoUrl = getTwitterImage(expandedUrl)
+                    if (photoUrl != -1)and(photoUrl!=[]):
+                        cnt += 1
+                        # print(photoUrl)
+                        tweetPhoto_dict.update({id: photoUrl})
+                        
+                        cur = conn.cursor()
+                        insert_query = """insert into images(id,image_url) values(%s,%s)"""
+                        for each_link in photoUrl:
+                            cur.execute(insert_query, (id, each_link))
+                        cur.close()
+                        conn.commit()
+                        
 
-            #else:
-                #print("the url does NOT link to a tweet with photo")
-        if cnt == 200:
-            break
+                break
+            except:
+                break
 
-    print(tweetPhoto_dict)
-json.dump(tweetPhoto_dict,open("tweetPhoto_urls","w"),separators=(',\n', ': '))
-
+        # else:
+        # print("the url does NOT link to a tweet with photo")
+    conn.close()
+    #print(tweetPhoto_dict)
+#json.dump(tweetPhoto_dict, open("tweetPhoto_urls", "w"), separators=(',\n', ': '))

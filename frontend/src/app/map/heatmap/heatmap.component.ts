9ff3b758c413f542040d993c991fce22a2b58c27
AC-Unicorn <a894657305@qq.com>
2019-06-25 16:30:01 -0700

    Angular refactoring with 2 layers

--- frontend/src/app/map/heatmap/heatmap.component.ts
+++ frontend/src/app/map/heatmap/heatmap.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import 'leaflet/dist/leaflet.css';
-import {latLng, tileLayer} from 'leaflet';
-// import {$} from 'protractor';
-
 declare let L;
+import * as $ from 'jquery';
+import HeatmapOverlay from 'leaflet-heatmap/leaflet-heatmap.js';
+import {MapService} from '../../services/map-service/map.service';
+import 'leaflet-maskcanvas';
 
 @Component({
   selector: 'app-heatmap',
@@ -12,31 +13,12 @@ declare let L;
 })
 export class HeatmapComponent implements OnInit {
 
-  constructor() {
+  constructor(private mapService: MapService) {
   }
 
 
-  // options = {
-  //   layers: [
-  //     // tslint:disable-next-line:max-line-length
-  //     tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  //       attribution: '&copy; OpenStreetMap contributors'
-  //     })
-  //   ],
-  //   zoom: 7,
-  //   center: latLng([ 46.879966, -121.726909 ])
-  // };
 
   ngOnInit() {
-    // const map = L.map('map').setView([51.505, -0.09], 13);
-    //
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //   attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    // }).addTo(map);
-
-    // for the base map layers
-
-
     const mbAttr = 'Credit to yc';
     const mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiY' +
       'SI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
@@ -59,10 +41,9 @@ export class HeatmapComponent implements OnInit {
       '<span style =\'color:black\'>Dark</span>': dark
     };
 
-
     const mainControl = L.control.layers(baseLayers).addTo(map);
 
-// Coordinate
+    // Coordinate
 
     map.addEventListener('mousemove', (ev) => {
       const lat = ev.latlng.lat;
@@ -70,13 +51,59 @@ export class HeatmapComponent implements OnInit {
       // $('#mousePosition').html('Lat: ' + Math.round(lat * 100) / 100 + ' Lng: ' + Math.round(lng * 100) / 100);
     });
 
-// Overlay event listener
+    // Overlay event listener
     const addOverlayHandler = (event) => {
       if (event.name === 'fire tweet') {
       }
     };
 
     map.on('overlayadd', addOverlayHandler);
+
+
+    const heatmapConfig = {
+        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+        // if scaleRadius is false it will be the constant radius used in pixels
+        radius: 1,
+        maxOpacity: 0.5,
+        // scales the radius based on map zoom
+        scaleRadius: true,
+        // if set to false the heatmap uses the global maximum for colorization
+        // if activated: uses the data maximum within the current map boundaries
+        //   (there will always be a red spot with useLocalExtremas true)
+        useLocalExtrema: true,
+        latField: 'lat',
+        lngField: 'lng',
+        valueField: 'temperature'
+    };
+
+    // Create heatmap overaly for temperature data with heatmap configuration
+    const heatmapLayer = new HeatmapOverlay(heatmapConfig);
+    // Request heatmap data from server
+    this.mapService.getHeatmapData();
+    // Cache heat data in the frontend
+    $(window).on('heatDataLoaded', (event, data) => {
+      heatmapLayer.setData(data);
+      mainControl.addOverlay(heatmapLayer, 'temperature');
+    });
+
+    const tweetLayer = L.TileLayer.maskCanvas({
+        radius: 10,
+        useAbsoluteRadius: true,
+        color: '#000',
+        opacity: 1,
+        noMask: true,
+        lineColor: '#e25822'
+    });
+
+    // Request tweets data from server
+    this.mapService.getTweetsData();
+
+    $(window).on('tweetsLoaded', (event, data) => {
+        tweetLayer.setData(data.tweetData);
+        mainControl.addOverlay(tweetLayer, 'Fire tweet');
+    });
+
+
   }
 
 

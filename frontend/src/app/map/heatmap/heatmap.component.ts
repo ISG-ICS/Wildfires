c821ac058e41e3e5ea103a09ef303c099ce14e77
AC-Unicorn <a894657305@qq.com>
2019-06-27 15:02:36 -0700

    event emitter and listener change to angular js native

--- frontend/src/app/map/heatmap/heatmap.component.ts
+++ frontend/src/app/map/heatmap/heatmap.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit} from '@angular/core';
 import 'leaflet/dist/leaflet.css';
 declare let L;
 import * as $ from 'jquery';
@@ -13,125 +13,142 @@ import 'leaflet-maskcanvas';
 })
 export class HeatmapComponent implements OnInit {
 
+  private mainControl;
+  private tweetData;
+  private tweetLayer;
+  private liveTweetLayer;
+  private liveTweetBird = [];
+  private liveTweetMarkers;
+  private map;
+  @Input() timeRangeChange;
+
   constructor(private mapService: MapService) {
   }
-
-
 
   ngOnInit() {
     const mbAttr = 'Credit to yc';
     const mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiY' +
       'SI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
-
     const satellite = L.tileLayer(mbUrl, {id: 'mapbox.satellite', attribution: mbAttr});
     const streets = L.tileLayer(mbUrl, {id: 'mapbox.streets', attribution: mbAttr});
     const dark = L.tileLayer(mbUrl, {id: 'mapbox.dark'});
-    const map = L.map('map', {
+    this.map = L.map('map', {
       center: [33.64, -117.84],
       zoom: 5,
       layers: [satellite, streets, dark]
     });
-    let tweetData = [];
     const baseLayers = {
       '<span style =\'color:blue\'>Satellite</span>': satellite,
       '<span style =\'color:red\'>Streets</span>': streets,
       '<span style =\'color:black\'>Dark</span>': dark
     };
 
-    const mainControl = L.control.layers(baseLayers).addTo(map);
+    this.mainControl = L.control.layers(baseLayers).addTo(this.map);
 
     // Coordinate
-
-    map.addEventListener('mousemove', (ev) => {
+    this.map.addEventListener('mousemove', (ev) => {
       const lat = ev.latlng.lat;
       const lng = ev.latlng.lng;
       $('#mousePosition').html('Lat: ' + Math.round(lat * 100) / 100 + ' Lng: ' + Math.round(lng * 100) / 100);
     });
 
+    this.mapService.getHeatmapData();
+    // Cache heat data in the frontend
+    this.mapService.heatmapDataLoaded.subscribe( this.heatmapDataHandler);
+
+    this.mapService.getTweetsData();
+    this.mapService.tweetDataLoaded.subscribe( this.tweetDataHandler);
+
+    $('#liveTweetSwitch').on('click', this.liveTweetSwitchHandler);
+    $(window).on('timeRangeChange', this.timeRangeChangeHandler);
+  }
+
+  tweetDataHandler = (data) => {
+
+    // Tweets related layers
+    this.tweetLayer = L.TileLayer.maskCanvas({
+      radius: 10,
+      useAbsoluteRadius: true,
+      color: '#000',
+      opacity: 1,
+      noMask: true,
+      lineColor: '#e25822'
+    });
+    const tempData = [];
+    this.tweetData = data.tweetData;
+    data.tweetData.forEach(x => {
+      tempData.push([x[0], x[1]]);
+    });
+
+    this.tweetLayer.setData(tempData);
+    this.mainControl.addOverlay(this.tweetLayer, 'Fire tweet');
+
+    // Mockup sampling data for fire events
+    const fireEventList = [];
+
+    for (let i = 0; i < data.tweetData.length; i++) {
+      if (i % 1000 === 0) {
+        const point = [data.tweetData[i][0], data.tweetData[i][1]];
+        const size = Math.floor(Math.random() * 80);
+        const fireIcon = L.icon({
+          iconUrl: 'assets/image/pixelfire.gif',
+          iconSize: [ size, size],
+        });
+        const marker = L.marker(point, {icon: fireIcon}).bindPopup('I am on fire');
+        fireEventList.push(marker);
+      }
+    }
+
+    const fireEvents = L.layerGroup(fireEventList);
+    this.mainControl.addOverlay(fireEvents, 'Fire event');
+
+    // Mockup Data for liveTweetLayer
+    const birdIcon = L.icon({
+      iconUrl: 'assets/image/perfectBird.gif',
+      iconSize: [20, 20]
+    });
+
+    for (let i = 0; i < data.tweetData.length; i++) {
+      if (i % 2000 === 0) {
+        const point = [data.tweetData[i][0], data.tweetData[i][1]];
+        const marker = L.marker(point, {icon: birdIcon}).bindPopup('I am a live tweet');
+        this.liveTweetBird.push(marker);
+      }
+    }
+
+    this.liveTweetLayer = L.layerGroup(this.liveTweetBird);
+    // Show the live tweet switch when the layer is ready
+    $('.switch').css('display', 'inline-block');
+
+  }
+
+  heatmapDataHandler = (data) => {
     const heatmapConfig = {
-        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-        // if scaleRadius is false it will be the constant radius used in pixels
-        radius: 1,
-        maxOpacity: 0.5,
-        // scales the radius based on map zoom
-        scaleRadius: true,
-        // if set to false the heatmap uses the global maximum for colorization
-        // if activated: uses the data maximum within the current map boundaries
-        //   (there will always be a red spot with useLocalExtremas true)
-        useLocalExtrema: true,
-        latField: 'lat',
-        lngField: 'lng',
-        valueField: 'temperature'
+      // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+      // if scaleRadius is false it will be the constant radius used in pixels
+      radius: 1,
+      maxOpacity: 0.5,
+      // scales the radius based on map zoom
+      scaleRadius: true,
+      // if set to false the heatmap uses the global maximum for colorization
+      // if activated: uses the data maximum within the current map boundaries
+      //   (there will always be a red spot with useLocalExtremas true)
+      useLocalExtrema: true,
+      latField: 'lat',
+      lngField: 'lng',
+      valueField: 'temperature'
     };
 
     // Create heatmap overaly for temperature data with heatmap configuration
     const heatmapLayer = new HeatmapOverlay(heatmapConfig);
-    // Request heatmap data from server
-    this.mapService.getHeatmapData();
-    // Cache heat data in the frontend
-    $(window).on('heatDataLoaded', (event, data) => {
-      heatmapLayer.setData(data);
-      mainControl.addOverlay(heatmapLayer, 'Temperature');
-    });
-    // Tweets related layers
-    const tweetLayer = L.TileLayer.maskCanvas({
-        radius: 10,
-        useAbsoluteRadius: true,
-        color: '#000',
-        opacity: 1,
-        noMask: true,
-        lineColor: '#e25822'
-    });
+    heatmapLayer.setData(data.heatmapData);
+    this.mainControl.addOverlay(heatmapLayer, 'Temperature');
 
-    this.mapService.getTweetsData();
+  }
 
-    let liveTweet = L.layerGroup();
-    const liveTweetList = [];
-    const htmlSelectElements = $(window).on('tweetsLoaded', (event, data) => {
+  liveTweetSwitchHandler = (event) => {
 
-      const tempData = [];
-      tweetData = data.tweetData;
-      data.tweetData.forEach(x => {
-        tempData.push([x[0], x[1]]);
-      });
-
-      tweetLayer.setData(tempData);
-      mainControl.addOverlay(tweetLayer, 'Fire tweet');
-
-      const fireEventList = [];
-      for (let i = 0; i < data.tweetData.length; i++) {
-          if (i % 1000 === 0) {
-            const point = [data.tweetData[i][0], data.tweetData[i][1]];
-            const size = Math.floor(Math.random() * 80);
-            const fireIcon = L.icon({
-                iconUrl: 'assets/image/pixelfire.gif',
-                iconSize:     [ size, size],
-            });
-            const marker = L.marker(point, {icon: fireIcon}).bindPopup('I am on fire');
-            fireEventList.push(marker);
-          }
-      }
-
-      const fireEvents = L.layerGroup(fireEventList);
-      mainControl.addOverlay(fireEvents, 'Fire event');
-
-      for (let i = 0; i < data.tweetData.length; i++) {
-        if (i % 2000 === 0) {
-          const point = [data.tweetData[i][0], data.tweetData[i][1]];
-          const icon = L.icon({
-              iconUrl: 'assets/image/perfectBird.gif',
-              iconSize: [20, 20]
-          });
-          const marker = L.marker(point, {icon}).bindPopup('I am a live tweet');
-          liveTweetList.push(marker);
-        }
-      }
-
-      liveTweet = L.layerGroup(liveTweetList);
-      $('.switch').css('display', 'inline-block');
-    });
-
-    const liveTweetLayer = L.TileLayer.maskCanvas({
+    this.liveTweetMarkers = L.TileLayer.maskCanvas({
       radius: 10,
       useAbsoluteRadius: true,
       color: '#000',
@@ -140,44 +157,36 @@ export class HeatmapComponent implements OnInit {
       lineColor: '#e25822'
     });
 
-    function liveTweetHandler(ev) {
-      liveTweet.addTo(map);
-      const temp = [];
-      liveTweetList.forEach(x => {
-          temp.push([x._latlng.lat, x._latlng.lng]);
-      });
+    this.liveTweetLayer.addTo(this.map);
+    const birdCoordinates = [];
+    this.liveTweetBird.forEach(x => {
+      birdCoordinates.push([x._latlng.lat, x._latlng.lng]);
+    });
 
-      liveTweetLayer.setData(temp);
-      const birds = $('.leaflet-marker-icon');
-      window.setTimeout(() => {
-          liveTweet.clearLayers();
-          liveTweet.addLayer(liveTweetLayer);
-      }, 3200);
-      let bird: any = 0;
-      for ( bird of birds) {
-        if (bird.src.indexOf('perfectBird') !== -1) {
-          $(bird).css('animation', 'fly 3s linear');
-        }
+    this.liveTweetMarkers.setData(birdCoordinates);
+    const birds = $('.leaflet-marker-icon');
+    window.setTimeout(() => {
+      this.liveTweetLayer.clearLayers();
+      this.liveTweetLayer.addLayer(this.liveTweetMarkers);
+    }, 3200);
+    let bird: any = 0;
+    for ( bird of birds) {
+      if (bird.src.indexOf('perfectBird') !== -1) {
+        $(bird).css('animation', 'fly 3s linear');
       }
     }
-
-    $('#liveTweetSwitch').on('click', liveTweetHandler);
-
-
-    const timeRangeHandler = (ev, data) => {
-      const tempData = [];
-      tweetData.forEach(entry => {
-        if (entry[2] > data.timebarStart && entry[2] < data.timebarEnd) {
-          tempData.push([entry[0], entry[1]]);
-        }
-      });
-      tweetLayer.setData(tempData);
-    };
-
-
-
-    $(window).on('timeRangeChange', timeRangeHandler);
   }
+
+  timeRangeChangeHandler = (event,data) => {
+    const tempData = [];
+    this.tweetData.forEach(entry => {
+      if (entry[2] > data.timebarStart && entry[2] < data.timebarEnd) {
+        tempData.push([entry[0], entry[1]]);
+      }
+    });
+    this.tweetLayer.setData(tempData);
+  }
+
 
 
 }
--- frontend/src/app/map/time-series/time-series.component.ts
+++ frontend/src/app/map/time-series/time-series.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import * as $ from 'jquery';
-declare var require: any
-const Highcharts = require('highcharts')
+import {MapService} from '../../services/map-service/map.service';
+declare var require: any;
+const Highcharts = require('highcharts');
 
 @Component({
   selector: 'app-time-series',
@@ -10,48 +11,53 @@ const Highcharts = require('highcharts')
 })
 export class TimeSeriesComponent implements OnInit {
 
-  constructor() { }
+  @Output() timeRangeChange = new EventEmitter();
+
+  constructor(private mapService: MapService) { }
 
   ngOnInit() {
-    $(window).on('timebarLoaed', (ev, data) => {
-      const chartData = data.chartData;
-      const timeseries = Highcharts.chart('timebar-container', {
-        chart: {
-          type: 'line',
-          zoomType: 'x',
-          height: 200,
-          backgroundColor: null,
-          events: {
-            selection(event) {
-              let timebarStart = 1;
-              let timebarEnd = 0;
-              if (event.hasOwnProperty('xAxis')) {
-                timebarStart = event.xAxis[0].min;
-                timebarEnd = event.xAxis[0].max;
-              } else {
-                timebarStart = event.target.axes[0].dataMin;
-                timebarEnd = event.target.axes[0].dataMax;
-              }
-              $(window).trigger('timeRangeChange', {timebarStart, timebarEnd});
-            }
-          }
-        },
-        title: {
-          text: 'Wildfire',
-          style: {
-            color: '#e25822'
-          }
-        },
-        xAxis: {
-          type: 'datetime'
-        },
-        series: [{
-          data: chartData,
-          color: '#e25822',
-          name: '<span style=\'color:#e25822\'>Wildfire Tweet</span>'
-        }]
+    this.mapService.timeseriesDataLoaded.subscribe(this.drawTimeSeries);
+  }
 
-      });
+  // Draw time series
+  drawTimeSeries = (data) => {
+    const chartData = data.chartData;
+    const that = this;
+    const timeseries = Highcharts.chart('timebar-container', {
+      chart: {
+        type: 'line',
+        zoomType: 'x',
+        height: 200,
+        backgroundColor: null,
+        events: {
+          selection(event) {
+            let timebarStart = 1;
+            let timebarEnd = 0;
+            if (event.hasOwnProperty('xAxis')) {
+              timebarStart = event.xAxis[0].min;
+              timebarEnd = event.xAxis[0].max;
+            } else {
+              timebarStart = event.target.axes[0].dataMin;
+              timebarEnd = event.target.axes[0].dataMax;
+            }
+            $(window).trigger('timeRangeChange', {timebarStart, timebarEnd});
+          }
+        }
+      },
+      title: {
+        text: 'Wildfire',
+        style: {
+          color: '#e25822'
+        }
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+      series: [{
+        data: chartData,
+        color: '#e25822',
+        name: '<span style=\'color:#e25822\'>Wildfire Tweet</span>'
+      }]
 
     });
   }

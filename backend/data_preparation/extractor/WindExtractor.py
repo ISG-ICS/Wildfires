cbeb80c61b47bfe27186325ac01ad5cd137824a8
amoxicillin <10252324+Shadow-Priest@users.noreply.github.com>
2019-07-06 19:41:59 -0700

    make the code formal

--- backend/data_preparation/extractor/WindExtractor.py
+++ backend/data_preparation/extractor/WindExtractor.py
@@ -1,4 +1,5 @@
 from backend.data_preparation.extractor.extractorbase import ExtractorBase
+from configurations import WIND_DATA_DIR, GRIB2_DATA_DIR, GRIB2JSON_PATH
 import os
 import subprocess
 import json
@@ -8,33 +9,34 @@ class WindExtractor(ExtractorBase):
     def __init__(self, filename, prop_name, prop_typeOfLevel):
         super().__init__(filename)
 
+    # may use GribConverter or grib2json to convert to json file.
     def extract(self, stamp, useJavaConverter):
         # make dir
-        if not os.path.isdir(os.path.join('backend', 'data')):
-            os.makedirs(os.path.join('backend', 'data'))
+        if not os.path.isdir(WIND_DATA_DIR):
+            os.makedirs(WIND_DATA_DIR)
         if useJavaConverter:
             # use grib2json
-            cmd = [os.path.join('converter', 'bin', 'grib2json'), '--data', '--output', 'backend/data/latest.json',
-                   '--names',
-                   '--compact', 'grib-data/' + stamp + '.f000']
+            cmd = [GRIB2JSON_PATH, '--data', '--output',
+                   os.path.join(WIND_DATA_DIR, 'latest.json'), '--names', '--compact',
+                   os.path.join(GRIB2_DATA_DIR, stamp + '.f000')]
             process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
             process.wait()
             if process.returncode == 0:
                 print('converted')
         else:
-            # or use GribConverter
+            # use GribConverter
             try:
-                from backend.data_preparation.extractor.GribConvertor import GribConvertor  # deferred import
-                j = GribConvertor.convert(os.path.join('grib-data', stamp + '.f000'))
-                with open(os.path.join('backend', 'data', 'latest.json'), 'w') as f:
-                    json.dump(j, f)
-                print('converted')
+                from backend.data_preparation.extractor.GribConverter import GribConverter  # deferred import
+                self.data = GribConverter.convert(os.path.join(GRIB2_DATA_DIR, stamp + '.f000'))
+                self.export('json', 'latest.json')
             except ModuleNotFoundError as e:
                 print(e)
                 print("\n\tpygrib is not supported on Windows, please use '-j' to use grib2json\n")
 
     def export(self, file_type: str, file_name: str) -> None:
-        pass
+        with open(os.path.join(WIND_DATA_DIR, file_name), 'w') as f:
+            json.dump(self.data, f)
+        print('converted')
 
     def __getitem__(self, index):
-        pass
+        return self.data.get(index)
--- configurations.py
+++ configurations.py
@@ -9,3 +9,9 @@ DATABASE_CONFIG_PATH = os.path.join(ROOT_DIR, 'configs/database.ini')
 NLTK_MODEL_PATH = os.path.join(ROOT_DIR, 'backend/models/nltk_model.pickle')
 
 TEST_DATA_PATH = os.path.join(ROOT_DIR, 'data/test')
+
+GRIB2_DATA_DIR = os.path.join(ROOT_DIR, 'data', 'grib-data')
+
+WIND_DATA_DIR = os.path.join(ROOT_DIR, 'backend', 'data')
+
+GRIB2JSON_PATH = os.path.join('converter', 'bin', 'grib2json')

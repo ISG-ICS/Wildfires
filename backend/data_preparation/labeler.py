2c64321dc7082d76667d6a1bdcb042084f9e24b5
Yicong-Huang <hyc541978023@gmail.com>
2019-07-08 13:25:13 -0700

    added support for "NOT SURE" value in labelling

--- backend/data_preparation/labeler.py
+++ backend/data_preparation/labeler.py
@@ -8,6 +8,8 @@ from backend.data_preparation.connection import Connection
 
 
 class Labeler:
+    labels = {0:"TRUE", 1: "FALSE", 2: "NOT_SURE"}
+
     def __init__(self, role):
         self.role = role
         self.conn = Connection()()
@@ -16,7 +18,7 @@ class Labeler:
     def mark(self, tweet_id, value) -> None:
         try:
             cur = self.conn.cursor()
-            sql = 'UPDATE records SET label1 = %s WHERE id = %s;'
+            sql = f'UPDATE records SET label{self.role} = {value} WHERE id = {tweet_id};'
             cur.execute(sql, (value, tweet_id))
             cur.close()
             self.conn.commit()
@@ -43,30 +45,34 @@ class Labeler:
         next_batch = self.get_next_unlabeled()
         while next_batch:
             for id, text in next_batch:
-                char = None
-                while not char:
-                    print(
-                        f'================================================\n\n\n\n{text}\n\n\n\n\n\n\n\n\n\n([1] for True, [r] for reverse previous, enter for next, other for False) ->')
-
-                    char = input().strip()
+                char = self.get_next_char(text)
 
                 while char == 'r':
                     if prev_text:
                         self.mark(prev_id, not prev_label)
-                        print(f"[{prev_text} is changed to {not prev_label}]")
-                        prev_label = not prev_label
+                        prev_label = (prev_label + 1) % 3
+                        print(f"[{prev_text} is changed to {self.labels[prev_label]}]")
                         self.mark(prev_id, prev_label)
+                    char = self.get_next_char(text)
 
-                    print(
-                        f'================================================\n\n\n\n{text}\n\n\n\n\n\n\n\n\n\n([1] for True, [r] for reverse previous, enter for next, other for False) ->')
-                    char = input().strip()
-                label = bool(char == '1')
-                print(label)
+                label = int(char) - 1
+
+                print(self.labels[label])
                 self.mark(id, label)
                 prev_label = label
                 prev_id = id
                 prev_text = text
             next_batch = self.get_next_unlabeled()
+
+    @staticmethod
+    def get_next_char(text):
+        char = None
+        while not char or char not in list('123r'):
+            print(
+                f'================================================\n\n\n\n{text}\n\n\n\n\n\n\n\n\n\n([1] for True, [2] for False, [3] for not sure, [r] for reverse previous (rotate in three values), enter for skip to next) ->')
+
+            char = input().strip()
+        return char
 
 
 if __name__ == '__main__':

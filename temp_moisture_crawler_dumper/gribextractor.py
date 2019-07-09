0fce92be2af96466dc60126827df9b32931041fc
gutingxuan <gutingxuan@outlook.com>
2019-07-09 15:21:43 -0700

    add tif extractctor and grib extractor.

---
+++ temp_moisture_crawler_dumper/gribextractor.py
@@ -0,0 +1,41 @@
+import json
+import pygrib
+
+from backend.data_preparation.extractor.extractorbase import ExtractorBase
+
+
+class GRIBExtractor(ExtractorBase):
+    def __init__(self, filename: str):
+        super().__init__(filename)
+        self.file_handler = pygrib.open(filename)
+
+    def extract(self, prop_name: str, prop_typeOfLevel=None, prop_first=None, prop_second=None) -> dict:
+        if prop_typeOfLevel != None:
+            prop_msg = self.file_handler.select(name=prop_name, typeOfLevel=prop_typeOfLevel)[0]
+        if prop_first != None and prop_second != None:
+            prop_msg = self.file_handler.select(name=prop_name, scaledValueOfFirstFixedSurface=prop_first,
+                                                scaledValueOfSecondFixedSurface=prop_second)[0]
+        prop_dict = dict()  # creates a new dictionary to store data
+        prop_vals = prop_msg.values  # values under the started property
+        lats, lons = prop_msg.latlons()
+        for row_cnt in range(0, len(prop_vals)):
+            for col_cnt in range(0, len(prop_vals[row_cnt])):
+                prop_dict[str((lats[row_cnt][col_cnt], lons[row_cnt][col_cnt]))] = prop_vals[row_cnt][col_cnt]
+        return prop_dict  # the location coordinates are different, don't need to worry about duplicated keys
+
+    def export(self, file_type: str, file_name) -> None:  # json
+        if file_type == 'json':
+            json.dump(self.data, open(file_name, 'w'))
+        # TODO: support other file format like CSV
+
+    def __getitem__(self, lat_lng_pair: tuple) -> float:
+        return self.data.get(str(lat_lng_pair))
+
+
+if __name__ == '__main__':
+    grib_extractor = GRIBExtractor('cdas1.t00z.sfluxgrbf00.grib2_20190706.txt')
+    temperature = grib_extractor.extract(prop_name='Temperature', prop_typeOfLevel='surface')
+    moisture = grib_extractor.extract(prop_name='Liquid volumetric soil moisture (non-frozen)', prop_first=0,
+                                      prop_second=10)
+    print(temperature)
+    print(moisture)
---
+++ temp_moisture_crawler_dumper/tifextractor.py
@@ -0,0 +1,62 @@
+import json
+import os
+from typing import Dict
+
+from osgeo import gdal
+
+from backend.data_preparation.extractor.extractorbase import ExtractorBase
+
+
+class TIFExtractor(ExtractorBase):
+    def __init__(self, filename: str):
+        super().__init__(filename)
+        self.tif = gdal.Open(filename)
+
+    def extract(self):
+        gt = self.tif.GetGeoTransform()
+        x_min = gt[0]
+        x_size = gt[1]
+        y_min = gt[3]
+        y_size = gt[5]
+        im_width = self.tif.RasterXSize
+        im_height = self.tif.RasterYSize
+        ulx = x_min  # upper left x
+        uly = y_min  # upper left y
+        lrx = im_width * x_size + x_min  # lower right x
+        lry = im_height * y_size + y_min  # lower right y
+        ds = gdal.Translate('output.xyz', self.tif, projWin=[ulx, uly, lrx, lry])
+
+        dictionary = dict()  # creates a new dictionary to store data
+
+        xyzFile = open('output.xyz')
+        line = xyzFile.readline()
+        while line:
+            long = line.split()[0]
+            lat = line.split()[1]
+            value = line.split()[2]
+            if str(value) != '-999000000':
+                if 'z.full' in self.filename:  # for temperature data
+                    time = self.filename[20:28]
+                    dictionary[(lat, long, time)] = value
+                if 'w.full' in self.filename:  # for moisture data
+                    time = self.filename[7:15]
+                    dictionary[(lat, long, time)] = value
+            line = xyzFile.readline()
+
+        self.data: Dict = dictionary
+        os.remove('output.xyz')
+        return dictionary
+
+    def export(self, file_type: str, file_name) -> None:  # json
+        if file_type == 'json':
+            json.dump(self.data, open(file_name, 'w'))
+        # TODO: support other file format like CSV
+
+    def __getitem__(self, lat_lng_pair: tuple) -> float:
+        return self.data.get(str(lat_lng_pair))
+
+
+if __name__ == '__main__':
+    tif_extractor = TIFExtractor('w.full.20190519.tif')
+    dict = tif_extractor.extract()
+    print(dict)

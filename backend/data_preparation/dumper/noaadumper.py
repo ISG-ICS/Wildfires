eb11012518d929f43f8665e11da00e4e907301b3
amoxicillin <10252324+Shadow-Priest@users.noreply.github.com>
2019-07-11 15:09:41 -0700

    port the wind crawler to wind & temperature

--- backend/data_preparation/dumper/noaadumper.py
+++ backend/data_preparation/dumper/noaadumper.py
@@ -2,17 +2,17 @@ from backend.data_preparation.dumper.dumperbase import DumperBase
 from backend.data_preparation.connection import Connection
 import psycopg2.errors
 import psycopg2.extras
+import datetime
 
 
-class WindDumperGeom(DumperBase):
-    sql_insert = 'INSERT INTO "wind" (tid, gid, ugnd, vgnd) VALUES %s'
-    # sql_select_time = 'SELECT max(tid) from "wind_reftime"'
-    # sql_insert_time = 'INSERT INTO "wind_reftime" (reftime, tid) VALUES (%s, %s)'
+class NOAADumper(DumperBase):
+    sql_insert = 'INSERT INTO "noaa0p25" (tid, gid, ugnd, vgnd, tmp) VALUES %s'
+    sql_insert_time = 'INSERT INTO "noaa0p25_reftime" (reftime, tid) VALUES (%s, %s)'
 
     def __init__(self):
         super().__init__()
 
-    def insert_one(self, ugnd: dict, vgnd: dict, stamp: str):
+    def insert_one(self, ugnd: dict, vgnd: dict, tmp: dict, reftime: datetime, stamp: str):
         # insert one record into database
         # recording insert count number to self.inserted_count
 
@@ -24,17 +24,14 @@ class WindDumperGeom(DumperBase):
             data = list()
             gid = 0
             for key in ugnd.keys():
-                data.append((tid, gid, ugnd.get(key) + 0.0, vgnd.get(key) + 0.0))  # testing
-                # data.append(('POINT({} {})'.format(tup[0], tup[1]), gid))  # wind_geometry
+                data.append((tid, gid, ugnd.get(key) + 0.0, vgnd.get(key) + 0.0, tmp.get(key) + 0.0))  # testing
+                # data.append(('POINT({} {})'.format(tup[0], tup[1]), gid))  # create geometry mesh
                 gid += 1
 
-            # insert wind data
+            # insert data
             try:
-                # cur.execute(WindDumperGeom.sql_insert_time, (reftime, tid))
-                psycopg2.extras.execute_values(cur, WindDumperGeom.sql_insert, data, template=None, page_size=10000)
-                # cur.execute(WindDumperGeom.sql_insert, (
-                #     3389, json_data[0]['data'][999], json_data[1]['data'][999], 'POINT({} {})'.format(255, 600),
-                #     refTime))
+                cur.execute(NOAADumper.sql_insert_time, (reftime, tid))
+                psycopg2.extras.execute_values(cur, NOAADumper.sql_insert, data, template=None, page_size=10000)
             except psycopg2.errors.UniqueViolation:
                 print('\n\tDuplicated Key')
             else:

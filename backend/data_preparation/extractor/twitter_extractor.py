d964828eea2b476bfbd9e84106a700fe4c9553ef
kathyzhuang <kathyzhuang378@gmail.com>
2019-07-11 15:20:23 -0700

    Restructured and create a twitter crawler, extractor, and dumper (according to bases)

--- backend/data_preparation/extractor/twitter_extractor.py
+++ backend/data_preparation/extractor/twitter_extractor.py
@@ -15,13 +15,13 @@ class TweetExtractor(ExtractorBase):
         self.data: list = []
         self.id: int
 
-    def extract(self, data_from_crawler: List[Dict]):
-        # extracts useful information after being provided with original tweet data (similar to a filter)
+    def extract(self, data_from_crawler: List[Dict]) -> List:
+        """extracts useful information after being provided with original tweet data (similar to a filter)"""
         collected_ids = set()
         extracted_data_list = []
-
         for tweet_json_string in data_from_crawler:
-            tweet = json.loads(str(tweet_json_string))
+            # tweet_json_string is a list of dictionary
+            tweet: dict = json.loads(str(tweet_json_string))
             id = tweet.get('id')
             if not id:
                 continue
@@ -30,25 +30,26 @@ class TweetExtractor(ExtractorBase):
                 collected_ids.add(id)
 
                 # extracts (filters) the useful information
-                date_time = datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S %z %Y')
-                text = tweet["text"].replace("'", "''")
-                hashtags = [tag['text'] for tag in tweet["hashtags"]]
+                date_time: datetime = datetime.strptime(tweet["created_at"], '%a %b %d %H:%M:%S %z %Y')
+                text: str= tweet["text"].replace("'", "''")
+                hashtags: List[str] = [tag['text'] for tag in tweet["hashtags"]]
                 if tweet.get('place') is not None:
                     top_left, _, bottom_right, _ = tweet["place"]['bounding_box']['coordinates'][0]
                 else:
                     top_left = bottom_right = None
                     # where the geolocation does not exist
 
-                extracted_data_list += [
+                self.data += [
                     {'id': id, 'date_time': date_time, 'text': text, 'hashtags': hashtags, 'top_left': top_left,
                      'bottom_right': bottom_right}]
-        self.data = extracted_data_list
-        return extracted_data_list
+        return self.data
         # stores self.data and returns a reference of it
 
     def export(self, file_type: str, file_name: str) -> None:
+        """exports data with specified file type"""
         # for example, json
         replace_list = list(self.data)
+        # make replace_list equal to self.data and change it
         if file_type == 'json':
             for each_extractor_line in replace_list:
                 each_extractor_line['date_time'] = str(each_extractor_line['date_time'])

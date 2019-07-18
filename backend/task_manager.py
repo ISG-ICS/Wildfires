5fe3939766e49b177bc64f37cbd789ec84e2fd62
AC-Unicorn <a894657305@qq.com>
2019-07-18 21:20:30 +0800

    update task_manager

--- backend/task_manager.py
+++ backend/task_manager.py
@@ -1,7 +1,9 @@
 import threading
 import time
-import argparse
+import rootpath
+rootpath.append()
 from data_preparation.crawler.windcrawler import WindCrawler
+
 
 
 class task_manager(object):
@@ -21,25 +23,55 @@ class task_manager(object):
     and may cause unexpected behaviour if used!
      """
     running_threads = []
+    task_options = {}
+    task_option_id = 1
 
     @classmethod
-    def run(cls, targetfunc, thname, loop, interval, arglist=[]):
-        """Statrs a new thread and appends it to the running_threads list
-        along with the specified values.
-        Loop and interval needs to be specified even if you dont
-        want it to loop over. This is to avoid lot of keyword arguments
-        and possible confusion.
-        Example of starting a looping thread:
-            ThM.run(function,"MyThreadName",True,0.5,[1,2,"StringArguemnt"])
+    def add_task_option(cls,task_name,task_func,task_number):
+        '''
+        :param task_name: name of this task option
+        :param task_func: the runnable of this task
+        :param task_number: id of this next task eg. let's say we had a wind_crawler-1 is running, to make our second wind crawler's name unique
+        we increment this number, so next wind crawler task will be called wind_crawler-2
+        :return: None
+        '''
+        cls.task_options.__setitem__(cls.task_option_id,[task_name,task_func,task_number])
+        cls.task_option_id += 1
 
-        To stop it, use:
-            ThM.stop_loop("MyThreadName")
-        Note, a stopped thread cannot be started again!
+    @classmethod
+    def delete_task_option(cls,task_option_id):
+        '''
+        :param task_option_id: id of task option you want to delete
+        :return: None
+        '''
+        cls.task_options.pop(task_option_id)
 
-        Example of a single-shot thread:
-            ThM.run(function,"ThreadName",False,0.5,[1,2,"StringArgument"])
-            """
+    @classmethod
+    def task_option_to_string(cls):
+        '''
 
+        :return: formated tasks in the current task option dictionary
+        '''
+        to_return = ""
+        task_template = "[%d].%s%d "
+        for option in cls.task_options:
+            to_return += task_template%(option,cls.task_options[option][0],cls.task_options[option][2])
+
+        return to_return
+
+    @classmethod
+    def run(cls, task_option_id, loop, interval, arglist=[]):
+        '''
+
+        :param task_option_id: task id for task in the task option list
+        :param loop:  determine is this is a looped task
+        :param interval: time between each execution of the looped task
+        :param arglist: argument for the task
+        :return: None
+        '''
+
+        targetfunc = cls.task_options[task_option_id][1]
+        thname = cls.task_options[task_option_id][0]+str(cls.task_options[task_option_id][2])
         th = threading.Thread(target=cls._thread_runner_, args=(targetfunc, thname, interval, arglist))
         th.setDaemon(True)
         cls.running_threads.append([th, thname, loop])
@@ -93,29 +125,20 @@ class task_manager(object):
             targetfunc(*arglist)
 
 
-
-
-
-
 if __name__ == "__main__":
 
     wind_crawler = WindCrawler()
-
     task_manager = task_manager()
+    task_manager.add_task_option("windcrawler-",wind_crawler.start,0)
+    task_loop = False
 
-    #Add crawler to the crawlers menu, all crawler instance
-    crawlers = {
-        1:["windcrawler-",wind_crawler,0]
-    }
-    crawler_loop = False
-    crawler_interval = 0
     while True:
         print ("#"*80)
         print ("#"+ "".center(78," ")+"#")
         print ("#"+ "".center(78," ")+"#")
-        print ("#"+ "Welcome to wildfire Crawler Manager".center(78," ")+"#")
-        print ("#"+ "Update: Add wind crawler".center(78," ")+"#")
-        print ("#"+ "Version 0.1".center(78," ")+"#")
+        print ("#"+ "Welcome to wildfire Task Manager".center(78," ")+"#")
+        print ("#"+ "Update: Bind task options to task_manager class".center(78," ")+"#")
+        print ("#"+ "Version 0.2".center(78," ")+"#")
         print ("#"+ "Credit to Unicorn".center(78," ")+"#")
         print ("#"+ "".center(78," ")+"#")
         print ("#"+ "".center(78," ")+"#")
@@ -123,45 +146,44 @@ if __name__ == "__main__":
 
         #Clear finished thread
         task_manager.free_dead()
-        print("You have following crawler running in loop")
+        print("You have following task running in loop")
         for i,thread in enumerate(task_manager.running_threads):
             if (thread[2] == True):
                 print( "[%d]: %s"%(i, thread[1]))
-        print("Enter the crawler Number to stop the crawler")
+        print("Enter the task Number to stop the task")
         try:
-            stop_crawler_prompt = input("if you don't want to break the loop press any key to continue\n")
-            stop_crawler_prompt = int(stop_crawler_prompt)
-            task_manager.stop_loop(task_manager.running_threads[stop_crawler_prompt][1])
+            stop_task_prompt = input("if you don't want to break the loop press any key to continue\n")
+            stop_task_prompt = int(stop_task_prompt)
+            task_manager.stop_loop(task_manager.running_threads[stop_task_prompt][1])
         except:
-            print(" Skipped, no crawler been terminated ");
-        crawler_prompt = input("Which Crawler would you like to run [1].Wind [2].Temprature [3].Moisture [q] quit\n")
-        crawler_prompt = crawler_prompt.strip()
-        crawler_prompt = crawler_prompt.lower()
-        if crawler_prompt == 'q':
+            print(" Skipped, no task been terminated ");
+        task_prompt = input("Which task would you like to run "+task_manager.task_option_to_string()+" [q] quit\n")
+        task_prompt = task_prompt.strip()
+        task_prompt = task_prompt.lower()
+        if task_prompt == 'q':
             break;
         else:
-            crawler_prompt = int(crawler_prompt)
+            task_prompt = int(task_prompt)
 
-        loop_prompt = input("Would you like to run crawler in a loop? yes/no ([y]/[n]) or [q] for quit\n")
+        loop_prompt = input("Would you like to run task in a loop? yes/no ([y]/[n]) or [q] for quit\n")
         loop_prompt = loop_prompt.strip()
         loop_prompt = loop_prompt.lower()
 
         if loop_prompt == 'q':
             break
         elif loop_prompt == 'y':
-            crawler_loop = True
+            task_loop = True
 
         interval_prompt = input("Interval between each run enter a NUMBER of seconds\n")
         try:
             interval_prompt = int(interval_prompt)
-
-            task_manager.run(targetfunc=crawlers[crawler_prompt][1].start,thname=crawlers[crawler_prompt][0]+str(crawlers[crawler_prompt][2]),loop=crawler_loop,interval=interval_prompt)
-            # Increment id of this type of crawler
-            crawlers[crawler_prompt][2]+=1
-            print(crawlers[crawler_prompt][2])
-            print("Your crawler is running!")
+            task_manager.run(task_option_id=task_prompt,loop=task_loop,interval=interval_prompt)
+            # Increment number of user specified task
+            task_manager.task_options[task_prompt][2] += 1
+            print("Your task is running!")
         except Exception as e:
-            print("[Error] Your input is not all correct, the crawler has not started")
+            print(e)
+            print("[Error] Your input is not all correct, the task has not started")
 
 
 

f642d91f99024af98df4f0d3a416430a92e10dfb
RainyTong <littlelittletong@gmail.com>
2019-07-17 17:03:23 -0700

    finish e2m classifier(including automatically selecting from database) & dumper

--- backend/classifiers/event2mind_classifier.py
+++ backend/classifiers/event2mind_classifier.py
@@ -6,6 +6,8 @@ from allennlp.predictors.predictor import Predictor
 
 rootpath.append()
 from backend.classifiers.classifierbase import ClassifierBase
+from backend.data_preparation.dumper.event2mind_dumper import Event2MindDumper
+from backend.data_preparation.connection import Connection
 import paths
 
 
@@ -21,14 +23,32 @@ class Event2MindClassifier(ClassifierBase):
     REACTION_Y_TOKENS = 'oreact_top_k_predicted_tokens'
     REACTION_Y_PROB = 'oreact_top_k_log_probabilities'
 
+    def start(self, target: Optional[int] = None):
+        """get records from database and dump prediction results into database"""
+
+        # set up database connection
+        conn = Connection()()
+        cur = conn.cursor()
+        cur.execute("SELECT id, text  from records")
+        # set up event2mindDumper
+        event2mindDumper = Event2MindDumper()
+        # loop every record in database
+        while True:
+            id, text = cur.fetchone()
+            if (id, text) is None:
+                break
+            # get prediction result of text
+            prediction_dict = self.predict(text, target)
+            # dumpe prediction result into database
+            event2mindDumper.insert(prediction_dict, id, conn)
+
     def set_model(self, model: Union[object, str] = None) -> None:
-        # set up emotion predictor
+        """set up the model as emotion predictor"""
 
         if model:
             self.model = Predictor.from_path(model)
         else:
             try:
-
                 self.model = Predictor.from_path(paths.EVENT2MIND_MODEL_PATH)
             except FileNotFoundError:
                 print(f"Downloading event2mind model {self.URL_EVENT2MIND} to {paths.EVENT2MIND_MODEL_PATH}")
@@ -37,6 +57,8 @@ class Event2MindClassifier(ClassifierBase):
                 self.model = Predictor.from_path(paths.EVENT2MIND_MODEL_PATH)
 
     def predict(self, text: str, target: Optional[int] = None) -> Union[Dict, List, Tuple]:
+        """give text as an input to model, then get a prediction dictionary as an output"""
+
         predictions = self.model.predict(source=text)
 
         intent = predictions[Event2MindClassifier.INTENT_TOKENS]
@@ -54,6 +76,7 @@ class Event2MindClassifier(ClassifierBase):
         dict_y_reaction = {Event2MindClassifier.REACTION_Y_TOKENS: reactions_y,
                            Event2MindClassifier.REACTION_Y_PROB: probabilities_y}
 
+        # return different prediction result specified by target
         if target == Event2MindClassifier.X_INTENT:
 
             return dict_intent
@@ -81,9 +104,9 @@ if __name__ == '__main__':
     # if event2mind model doesn't exist locally, get model from url
     event2mindClassifier.set_model(Event2MindClassifier.URL_EVENT2MIND)
 
-    # predict a text like "hello", specify what to extract: X_INTENT or X_REACTION or Y_REACTION
-    dict_intent = event2mindClassifier.predict("hello", event2mindClassifier.X_INTENT)
-    print(dict_intent)
+    # get from database one record a time, use event2mind model to predict and dump results to database
+    # specify what to get & dump: X_INTENT or X_REACTION or Y_REACTION
+    event2mindClassifier.start(event2mindClassifier.X_INTENT)
 
-    # if not specified, output all the results including x's intents, x's reactions and y's reactions
-    print(event2mindClassifier.predict("hello"))
+    # if no specification, get & dump three of them:
+    event2mindClassifier.start()

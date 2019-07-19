858edc2c421cf4a93fdf972f3e239573f65aa4d5
RainyTong <littlelittletong@gmail.com>
2019-07-19 14:53:53 -0700

    Modify image classifier and image classification dumper, add image_classification.py

--- backend/data_preparation/dumper/img_classification_dumper.py
+++ backend/data_preparation/dumper/img_classification_dumper.py
@@ -2,22 +2,22 @@ import rootpath
 
 rootpath.append()
 from backend.data_preparation.dumper.dumperbase import DumperBase
+from backend.data_preparation.connection import Connection
 
 
 class ImgClassificationDumper(DumperBase):
 
-    def insert(self, data, id, conn):
+    def insert(self, data, id):
         '''
         data: image prediction result -- probability of being wildfire and not wildfire
         insert image prediction result into images table
         '''
-        cur = conn.cursor()
+
         try:
             sql = """ UPDATE images
                             SET not_wildfire_prob = %s, wildfire_prob = %s
                             WHERE id = %s"""
-            cur.execute(sql, (data[0], data[1], id,))
+            Connection().sql_execute_values(sql, [data[0], data[1], id])
+
         except Exception as err:
             print("error", err)
-        conn.commit()
-        cur.close()

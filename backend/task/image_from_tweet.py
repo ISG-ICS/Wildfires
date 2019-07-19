ec220b386ff65fde7af58a6880b249fe05d0443c
Yicong-Huang <hyc541978023@gmail.com>
2019-07-18 17:03:10 -0700

    restructure tweet url extractor and make a runnable

---
+++ backend/task/image_from_tweet.py
@@ -0,0 +1,25 @@
+import rootpath
+
+from backend.data_preparation.connection import Connection
+from backend.data_preparation.dumper.url_dumper import URLDumper
+from backend.data_preparation.extractor.tweet_media_extractor import TweetMediaExtractor
+
+rootpath.append()
+from backend.task.runnable import Runnable
+
+
+class ImageFromTweet(Runnable):
+
+    def __init__(self):
+        self.extractor = TweetMediaExtractor()
+        self.dumper = URLDumper()
+
+    def run(self, batch_num=100):
+        self.dumper.insert({id: self.extractor.extract(text) for id, text in
+                            Connection().sql_execute(
+                                f"select id, text from records r WHERE NOT EXISTS (select distinct id from images i where i.id = r.id) limit {batch_num}")})
+
+
+if __name__ == '__main__':
+    while True:
+        ImageFromTweet().run()

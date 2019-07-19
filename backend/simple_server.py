a419df0191548b18868aedf02f5470581ede6e27
amoxicillin <10252324+Shadow-Priest@users.noreply.github.com>
2019-07-19 11:50:52 -0700

    update simple_server

--- backend/simple_server.py
+++ backend/simple_server.py
@@ -10,14 +10,22 @@ import json
 import requests
 import re
 import string
-import pygrib
-import numpy as np
+from ast import literal_eval as make_tuple
 
-from flask import Flask, send_from_directory, make_response, jsonify
+from flask import Flask, send_from_directory, make_response, jsonify, request as flask_request
 
 from paths import NLTK_MODEL_PATH
 
 app = Flask(__name__, static_url_path='')
+app.config.from_pyfile('server_config.py', silent=True)
+
+with open('us_states_dict.json', 'r') as f:
+    json_str = f.read()
+    us_states = json.loads(json_str)  # type: dict
+
+with open('us_states_abbr.json', 'r') as f:
+    json_str = f.read()
+    us_states_abbr = json.loads(json_str)  # type: dict
 
 nl: NLTKTest = pickle.load(open(NLTK_MODEL_PATH, 'rb'))
 api = twitter.Api(consumer_key="",
@@ -31,50 +39,96 @@ tweet_query = "select r.create_at, l.top_left_long, l.top_left_lat, l.bottom_rig
 
 @app.route("/temp")
 def send_temp_data():
-    query = "select * from recent_temperature "
+    # query = "select * from recent_temperature "
+    query = "SELECT * from Polygon_Aggregator_noaa0p25(%s, %s)"
+    northwest = make_tuple(flask_request.args.get('northwest'))
+    southeast = make_tuple(flask_request.args.get('southeast'))
+    tid = flask_request.args.get('tid')
+    poly = 'polygon(({0} {1}, {0} {2}, {3} {2}, {3} {1}, {0} {1}))'.format(northwest[0], northwest[1], southeast[1],
+                                                                           southeast[0])
     with Connection() as conn:
         cur = conn.cursor()
-        cur.execute(query)
+        cur.execute(query, (poly, tid))
         resp = make_response(
-            jsonify([{"lng": long, "lat": lat, "temperature": value} for lat, long, value, _ in cur.fetchall()]))
+            jsonify([{"lng": long, "lat": lat, "temperature": value} for lat, long, value in cur.fetchall()]))
         resp.headers['Access-Control-Allow-Origin'] = '*'
         cur.close()
     return resp
 
 
+@app.route("/search")
+def send_search_data():
+    keyword = flask_request.args.get('keyword')
+    search_result = None
+    if keyword in us_states:
+        search_result = us_states[keyword]
+    elif keyword in us_states_abbr:
+        search_result = us_states[us_states_abbr[keyword]]
+    resp = make_response(jsonify(search_result['geometry'])) if search_result else make_response(jsonify(None))
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+
+@app.route("/boundaries", methods=['POST', 'GET'])
+def send_boundaries_data():
+    request_json = flask_request.get_json(force=True)
+    states = request_json['states']
+    cities = request_json['cities']
+    north = request_json['northEast']['lat']
+    east = request_json['northEast']['lon']
+    south = request_json['southWest']['lat']
+    west = request_json['southWest']['lon']
+
+    select_states = "SELECT * from select_states_intersects(%s)"
+    select_cities = "SELECT * from select_cities_intersects(%s)"
+    poly = 'polygon(({0} {1}, {0} {2}, {3} {2}, {3} {1}, {0} {1}))'.format(north, west, east, south)
+    with Connection() as conn:
+        cur = conn.cursor()
+
+        if states:
+            cur.execute(select_states, (poly,))
+        elif cities:
+            cur.execute(select_cities, (poly,))
+        resp = make_response(
+            jsonify([{"geometry": geom2dict(geom)} for _, geom in cur.fetchall()]))
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        cur.close()
+    return resp
+
+
+def geom2dict(geom: str) -> dict:
+    geometry = dict()
+
+    if geom[0] == 'P':
+
+        geometry['type'] = 'Polygon'
+        coordinates = list()
+        pairs = geom[geom.find('((') + 2:-2]
+        pairs = pairs.split(',')
+        for pair in pairs:
+            lat, lon = pair.split(' ')
+            coordinates.append([float(lat), float(lon)])
+        geometry['coordinates'] = [coordinates, ]
+    else:
+        geometry['type'] = 'MultiPolygon'
+        coordinates = list()
+        polygons = geom[geom.find('((') + 3:-3]
+        polygons = polygons.split('),(')
+        for polygon in polygons:
+            coordinates_this = list()
+            pairs = polygon.split(',')
+            for pair in pairs:
+                lat, lon = pair.split(' ')
+                coordinates_this.append([float(lat), float(lon)])
+            coordinates.append(coordinates_this)
+        geometry['coordinates'] = [coordinates, ]
+
+    return geometry
+
+
 @app.route("/wind")
 def send_wind_data():
-    grbs = pygrib.open('gfs.t12z.pgrb2.0p25.anl')
-    grbs.seek(0)
-    result_list = []
-    result = {}
-    result['header'] = {}
-    result['data'] = np.array([])
-    for g in grbs:
-        # OPTIMIZE: duplication with grib_converter.py
-        for row in g['values']:
-            result['data'] = np.concatenate((result['data'], row), axis=0)
-
-        result['header'] = {
-            'parameterCategory': g["parameterCategory"],
-            'parameterNumber': g['parameterNumber'],
-            'numberPoints': len(result['data']),
-            'nx': g['Ni'],
-            'ny': g['Nj'],
-            'lo1': g['longitudeOfFirstGridPointInDegrees'],
-            'lo2': g['longitudeOfLastGridPointInDegrees'],
-            'la1': g['latitudeOfFirstGridPointInDegrees'],
-            'la2': g['latitudeOfLastGridPointInDegrees'],
-            'dx': g['iDirectionIncrementInDegrees'],
-            'dy': g['jDirectionIncrementInDegrees'],
-        }
-        result['data'] = result['data'].tolist()
-        result_list.append(result)
-        result = {}
-        result['header'] = {}
-        result['data'] = []
-        resp = make_response(jsonify(result_list))
-    # resp = make_response(send_from_directory('','2019070312.json'))
+    resp = make_response(send_from_directory('data', 'latest-wind.json'))
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 

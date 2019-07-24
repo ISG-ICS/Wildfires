34236342a3eb0b1c48da4872db1b609201d936ea
Yicong-Huang <hyc541978023@gmail.com>
2019-07-24 07:36:48 -0700

    removed unused `nouislider`

--- frontend/src/app/map/sidebar/temperature-range-slider/temperature-range-slider.component.ts
+++ frontend/src/app/map/sidebar/temperature-range-slider/temperature-range-slider.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {MapService} from '../../../services/map-service/map.service';
-import 'nouislider/distribute/nouislider.css';
 
 
 @Component({

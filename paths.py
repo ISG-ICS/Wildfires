e6fb60c29a28b1dab185bba207b5f8354b2d5e59
HugoQN <qiaonanh@uci.edu>
2019-07-24 13:54:23 -0700

    CONFIG_PATH in paths

--- paths.py
+++ paths.py
@@ -13,6 +13,8 @@ CONFIGS_DIR = os.path.join(ROOT_DIR, 'configs')
 DATABASE_CONFIG_PATH = os.path.join(CONFIGS_DIR, 'database.ini')
 # path of the config file for twitter api
 TWITTER_API_CONFIG_PATH = os.path.join(CONFIGS_DIR, 'twitter.ini')
+# path of the config file for logging in task manager
+LOG_CONFIG_PATH = os.path.join(CONFIGS_DIR, 'dict_config.json')
 
 NLTK_MODEL_PATH = os.path.join(ROOT_DIR, 'backend/models/nltk_model.pickle')
 

1e9e6c2cbe2d5ef99e929699184b136216694ab8
kathyzhuang <kathyzhuang378@gmail.com>
2019-07-24 16:53:42 -0700

    constructed the SearchBox which allows user to search for location using the search box. Also highlights the boundaries when the user moves mouse to a certain area.

--- frontend/src/app/map/map.module.ts
+++ frontend/src/app/map/map.module.ts
@@ -1,16 +1,19 @@
-import {Input, NgModule} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
 import {HeatmapComponent} from './heatmap/heatmap.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { TimeSeriesComponent } from './time-series/time-series.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {TimeSeriesComponent} from './time-series/time-series.component';
+import {SearchComponent} from "./search/search.component";
+
 
 @NgModule({
-  declarations: [HeatmapComponent, SidebarComponent, TimeSeriesComponent],
+    declarations: [HeatmapComponent, SidebarComponent, TimeSeriesComponent, SearchComponent],
   exports: [
     HeatmapComponent,
     SidebarComponent,
     TimeSeriesComponent,
+      SearchComponent,
   ],
   imports: [
     CommonModule,

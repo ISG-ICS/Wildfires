1bfb44b982b0e3233deaa1c9b78c2693223a65d2
Yicong-Huang <hyc541978023@gmail.com>
2019-07-27 14:47:05 -0700

    deprecated __call__, rearrange the methods. left support for Connection().sql_execute* left support for Connection()()

--- backend/data_preparation/connection.py
+++ backend/data_preparation/connection.py
@@ -3,6 +3,7 @@ from typing import Generator, Tuple, Any, List
 
 import psycopg2.pool
 import rootpath
+from deprecated import deprecated
 
 rootpath.append()
 from paths import DATABASE_CONFIG_PATH
@@ -17,13 +18,24 @@ class Connection:
         if not Connection._pool:
             Connection._pool = psycopg2.pool.ThreadedConnectionPool(1, 5, **self.config())
 
-    def __enter__(self):
-        self.conn = Connection._pool.getconn()
+    def __enter__(self, *args, **kwargs):
+        """Context Manager enter point, returns an available connection from the _pool"""
+        # TODO: save the reference to thread-name or other identifiers (using getconn(key=Thread-name)
+        self.conn = Connection._pool.getconn(*args, **kwargs)
         self.get_connection_status(self.conn)
         return self.conn
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        """Context Manager exit point, put the occupied connection back into the _pool"""
         Connection._pool.putconn(self.conn)
+
+    @staticmethod
+    @deprecated(reason="__call__ will no longer be provided in future, please always use context manager (with)")
+    def __call__(*args, **kwargs):
+        """returns a newly created connection, which is not maintained by the _pool"""
+        connection = psycopg2.connect(*args, **Connection.config(), **kwargs)
+        Connection.get_connection_status(connection)
+        return connection
 
     @staticmethod
     def config():
@@ -42,64 +54,63 @@ class Connection:
             f"= {connection_count}, MAXIMUM = {connection_max_count}")
         cursor.close()
 
-    # not recommended: connection would not be recycled and user should close it manually
-    def __call__(self, *args, **kwargs):
-        connection = psycopg2.connect(**self.config())
-        Connection.get_connection_status(connection)
-        return connection
-
-    def sql_execute(self, sql: str) -> Generator[Tuple[Any], None, None]:
+    @staticmethod
+    def sql_execute(sql: str) -> Generator[Tuple[Any], None, None]:
         """to execute an SQL query and iterate the output"""
         print(f"SQL: {sql}")
         if any([keyword in sql.upper() for keyword in ["INSERT", "UPDATE"]]):
             print("You are running INSERT or UPDATE without committing, transaction aborted. Please retry with "
                   "sql_execute_commit")
             return
-        with self() as connection:
+        with Connection() as connection:
             cursor = connection.cursor()
             cursor.execute(sql)
 
             try:
-                row = cursor.fetchone()
-                while row:
-                    yield row
-                    row = cursor.fetchone()
+                one_row = cursor.fetchone()
+                while one_row:
+                    yield one_row
+                    one_row = cursor.fetchone()
             except psycopg2.ProgrammingError:
                 pass
             finally:
                 cursor.close()
 
-    def sql_execute_commit(self, sql: str) -> None:
+    @staticmethod
+    def sql_execute_commit(sql: str) -> None:
         """to execute and commit an SQL query"""
         print(f"SQL: {sql}")
-        with self() as connection:
+        with Connection() as connection:
             cursor = connection.cursor()
             cursor.execute(sql)
             connection.commit()
             print(f"     Affected rows:{cursor.rowcount}")
             cursor.close()
 
-    def sql_execute_values(self, sql: str, value_tuples: List[Tuple[Any]], ignore_duplicate: bool = True):
+    @staticmethod
+    def sql_execute_values(sql: str, value_tuples: List[Tuple[Any]], ignore_duplicate: bool = True) -> None:
+        """to execute and commit an SQL query with multiple value tuples"""
         value_tuples_sql = ", ".join(
             [f"({', '.join([repr(str(entry)) if isinstance(entry, datetime) else repr(entry) for entry in entries])})"
              for entries in value_tuples])
         if value_tuples_sql:
             sql += " " + value_tuples_sql + f" ON CONFLICT DO NOTHING" if ignore_duplicate else ""
-            self.sql_execute_commit(sql)
+            Connection.sql_execute_commit(sql)
         else:
             print("[DATABASE] Nothing to commit")
 
 
 if __name__ == '__main__':
-    # use as a context manager
+    # use as a context contextmanager
     with Connection() as conn:
         cur = conn.cursor()
         cur.execute("select * from pg_tables")
         print(cur.fetchall())
         cur.close()
 
-    # # not recommended: connection would not be recycled and user should close it manually
-    conn = Connection()()  # call the object to return a connection from pool
+    # use a non-managed Connection, not maintained by connection pool, need to be closed manually.
+    # TODO: remove the support for such usage, once other depended modules are updated.
+    conn = Connection()()  # call the object to return a new connection
     cur = conn.cursor()
     cur.execute("select * from pg_tables")
     print(cur.fetchall())
@@ -107,5 +118,9 @@ if __name__ == '__main__':
     conn.close()  # user should close it manually
 
     # execute sql directly, with output fetched iteratively
+    for row in Connection.sql_execute("select * from pg_tables"):
+        print(row)
+
+    # remains supported for now.
     for row in Connection().sql_execute("select * from pg_tables"):
         print(row)

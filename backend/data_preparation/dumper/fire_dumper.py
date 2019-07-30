28064bb489d845d07fd144f91e3737fee8547d44
shilingz <shilingz@uci.edu>
2019-07-30 02:55:22 -0700

    Runnable finished. Need to be test on server

--- backend/data_preparation/dumper/fire_dumper.py
+++ backend/data_preparation/dumper/fire_dumper.py
@@ -12,12 +12,18 @@ class FireDumper(DumperBase):
     Table 2(fire_geoms): firename firetime firegeom
     """
     sql_check_if_history_table_exists = 'SELECT table_name FROM information_schema.TABLES WHERE table_name = \'fire_crawl_history\''
-    sql_retrieve_all_fires = 'SELECT * FROM fire_names'
-    sql_create_history_table = 'CREATE TABLE IF NOT EXISTS fire_crawl_history (fireyear int4, firename VARCHAR (30))'
+    sql_create_history_table = 'CREATE TABLE IF NOT EXISTS fire_crawl_history (fireyear int4, firename VARCHAR (20), PRIMARY KEY (fireyear, firename))'
+    sql_retrieve_all_fires = 'SELECT * FROM fire_crawl_history'
+    sql_check_if_fire_info_table_exists = 'SELECT table_name FROM information_schema.TABLES WHERE table_name = \'fire_info\''
+    sql_create_fire_info_table = 'CREATE TABLE IF NOT EXISTS fire_info (firename VARCHAR (20), fire_if_sequence boolean, fireagency VARCHAR (20), firetime timestamp, firegeom polygon, PRIMARY KEY (firename, firetime))'
+    sql_insert_fire_into_history = 'INSERT INTO "fire_crawl_history" (fireyear, firename) VALUES (%(year)s, %(firename)s) ON CONFLICT DO NOTHING'
+    sql_insert_fire_into_info = 'INSERT INTO "fire_info" (firename, fire_if_sequence, fireagency, firetime, firegeom) VALUES (%(firename)s,%(if_sequence)s,%(agency)s,%(datetime)s,%(geopolygon)s) ON CONFLICT DO NOTHING'
+    sql_count_records = 'SELECT COUNT(*) FROM fire_crawl_history'
+
     def __init__(self):
         super().__init__()
 
-    def insert(self, ugnd: dict, vgnd: dict, tmp: dict, soilw: dict, reftime: datetime, stamp: str) -> None:
+    def insert(self) -> None:
         pass
 
     def check_history(self, conn):
@@ -33,6 +39,18 @@ class FireDumper(DumperBase):
             conn.commit()
         cur.close()
 
+    def check_info(self, conn):
+        """
+        create fire_crawl_history table if not exist
+        """
+        cur = conn.cursor()
+        # if table not exist
+        cur.execute(self.sql_check_if_fire_info_table_exists)
+        tables = cur.fetchall()
+        if len(tables) == 0:
+            cur.execute(FireDumper.sql_create_fire_info_table)
+            conn.commit()
+        cur.close()
 
     def retrieve_all_fires(self):
         """
@@ -45,4 +63,15 @@ class FireDumper(DumperBase):
             cur.execute(self.sql_retrieve_all_fires)
             result = cur.fetchall()
         return result
+
+    def insert(self, info: dict):
+        with Connection() as connect:
+            self.check_info(connect)
+            cur = connect.cursor()
+            cur.execute(self.sql_insert_fire_into_history, info)
+            cur.execute(self.sql_insert_fire_into_info, info)
+            connect.commit()
+            cur.execute(self.sql_count_records)
+            self.inserted_count = cur.fetchone()[0]
+            cur.close()
 
--- backend/data_preparation/extractor/fire_extractor.py
+++ backend/data_preparation/extractor/fire_extractor.py
@@ -1,5 +1,6 @@
 import rootpath
 rootpath.append()
+import re
 from backend.data_preparation.extractor.extractorbase import ExtractorBase
 from paths import FIRE_DATA_DIR
 from typing import Dict
@@ -11,18 +12,71 @@ class FireExtractor(ExtractorBase):
         super().__init__()
 
 
+    def extract(self, path, record, if_sequence):
+        """
+        Extract useful information of a fire from a path
+        :param path: str, path of the files of this fire
+        :param record: str, the name of this record(stage)
+        :param if_sequence: bool, if the stage belongs to a sequence of fire
+        :return: result: dict, all values needed
+        """
+        # The original data is not clean, field names are different each year
+        # this dictionary is for the extractor to get the exactly year's field names
+        # fieldsbyyear = {2010:("FIRE_NAME", "DATE_", "TIME_", "AGENCY"),
+        #                 2011:("FIRE_NAME", "DATE_", "TIME_", "AGENCY"),
+        #                 2012:("FIRE_NAME", "DATE_", "TIME_", "AGENCY"),
+        #                 2013:("FIRE_NAME", "DATE_", "TIME_", "AGENCY"),
+        #                 2014:("FIRE_NAME", "DATE_", "TIME_", "AGENCY"),
+        #                 2015:("FIRE_NAME", "DATE_", "TIME_", "AGENCY"),
+        #                 2016:("fireName", "perDatTime", "agency"),
+        #                 2017:("fireName", "perDatTime", "agency"),
+        #                 2018:("fireName", "perDatTime", "agency"),
+        #                 2019:("FIRENAME", "DATECRNT","AGENCY")}
+        # extract the year number from record and convert it to an int
+        year = int(re.search(r"\d{8}",record).group()[:4])
 
-    def extract(self, path, folder):
-        """
-        extract data from file to dict
-        :param sf:
-        :return:
-        """
-        data =[]
-        shp = shapefile.Reader(path + "/" + folder)
-        firetime = datetime.datetime.strptime(shp.records()[0]['perDatTime'], '%m/%d/%Y %I:%M:%S %p')
-        firename = shp.records()[0]['fireName'].capitalize()
-        firepolygon_x
+        # defining fields each year in dict is not proper since
+        # before 2016 there are 4 fields needed. But after 2016 there are only 3
+        # decided to use if statement
+
+        # NOTE: current year's schema is different from 2016-2018, not sure if it is a temp field names for
+        # current_year only or the names of fields starts to change again after 2018
+
+        # result to return -- a dict
+        result = dict()
+
+        # read the shp
+        shp = shapefile.Reader(path + "/" + record)
+
+        # fill result dict based on the format for this year
+        if year < 2016:
+            # FIRE_NAME, DATE_:  datetime.date(2014, 9, 11), TIME_: 0129, AGENCY: USFS or NULL
+            result["firename"] = shp.record(0)["FIRE_NAME"].capitalize()
+            result["agency"] = shp.record(0)["AGENCY"] if shp.record(0)["AGENCY"] != "" else "Unknown"
+            result["datetime"] = datetime.datetime.strptime("{:%m%d%Y}".format(shp.record(0)["DATE_"]) + \
+                                                            shp.records()[0]['TIME_'], '%m%d%Y%H%M')
+        else:
+            if year < 2019:
+                # fireName, perDatTime(maybe just date but no time), agency(might be null)
+                result["firename"] = shp.record(0)["fireName"].capitalize()
+                result["agency"] = shp.record(0)["agency"] if shp.record(0)["agency"] != "" else "Unknown"
+                result["datetime"] = datetime.datetime.strptime(shp.record(0)['perDatTime'], '%m/%d/%Y %I:%M:%S %p') if \
+                    len(shp.record(0)['perDatTime']) > 11 else datetime.datetime.strptime(shp.record(0)['perDatTime'], '%m/%d/%Y')
+            else:
+                # FIRENAME, DATECRNT, AGENCY
+                result["firename"] = shp.record(0)["FIRENAME"].capitalize()
+                result["agency"] = shp.record(0)["AGENCY"] if shp.record(0)["AGENCY"] != "" else "Unknown"
+                result["datetime"] = datetime.datetime.strptime(shp.record(0)['DATECRNT'], '%m/%d/%Y %I:%M:%S %p') if \
+                    len(shp.record(0)['DATECRNT']) > 11 else datetime.datetime.strptime(shp.record(0)['DATECRNT'], '%m/%d/%Y')
+
+        result["geopolygon"] = shp.shapeRecord(0).shape.points
+        result["year"] = year
+        result["if_sequence"] = if_sequence
+        return result
+
+    def export(self, file_type: str, file_name: str):
+        return
+
 
 
 

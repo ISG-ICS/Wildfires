7327857336fdb29153e06a00b20a70cafcb8dd97
shilingz <shilingz@uci.edu>
2019-07-31 17:07:57 -0700

    Need fix in function separate_multipart_shape.

--- backend/data_preparation/dumper/fire_dumper.py
+++ backend/data_preparation/dumper/fire_dumper.py
@@ -12,12 +12,12 @@ class FireDumper(DumperBase):
     Table 2(fire_geoms): firename firetime firegeom
     """
     sql_check_if_history_table_exists = 'SELECT table_name FROM information_schema.TABLES WHERE table_name = \'fire_crawl_history\''
-    sql_create_history_table = 'CREATE TABLE IF NOT EXISTS fire_crawl_history (fireyear int4, firename VARCHAR (20), PRIMARY KEY (fireyear, firename))'
+    sql_create_history_table = 'CREATE TABLE IF NOT EXISTS fire_crawl_history (year int4, name VARCHAR (40), PRIMARY KEY (year, name))'
     sql_retrieve_all_fires = 'SELECT * FROM fire_crawl_history'
     sql_check_if_fire_info_table_exists = 'SELECT table_name FROM information_schema.TABLES WHERE table_name = \'fire_info\''
-    sql_create_fire_info_table = 'CREATE TABLE IF NOT EXISTS fire_info (firename VARCHAR (20), fire_if_sequence boolean, fireagency VARCHAR (20), firetime timestamp, firegeom polygon, PRIMARY KEY (firename, firetime))'
-    sql_insert_fire_into_history = 'INSERT INTO "fire_crawl_history" (fireyear, firename) VALUES (%(year)s, %(firename)s) ON CONFLICT DO NOTHING'
-    sql_insert_fire_into_info = 'INSERT INTO "fire_info" (firename, fire_if_sequence, fireagency, firetime, firegeom) VALUES (%(firename)s,%(if_sequence)s,%(agency)s,%(datetime)s,%(geopolygon)s) ON CONFLICT DO NOTHING'
+    sql_create_fire_info_table = 'CREATE TABLE IF NOT EXISTS fire_info (name VARCHAR (40), if_sequence boolean, agency VARCHAR (20), time timestamp, geom geometry, PRIMARY KEY (name, time))'
+    sql_insert_fire_into_history = 'INSERT INTO "fire_crawl_history" (year, name) VALUES (%(year)s, %(firename)s) ON CONFLICT DO NOTHING'
+    sql_insert_fire_into_info = 'INSERT INTO "fire_info" (name, if_sequence, agency, time, geom) VALUES (%(firename)s,%(if_sequence)s,%(agency)s,%(datetime)s,%(geopolygon)s) ON CONFLICT DO NOTHING'
     sql_count_records = 'SELECT COUNT(*) FROM fire_crawl_history'
 
     def __init__(self):
@@ -68,18 +68,27 @@ class FireDumper(DumperBase):
 
     def insert(self, info: dict):
         print("Inserting fire:",info["firename"],info["datetime"])
-        s = "("
-        for t in info["geopolygon"]:
-            s += "({},{}),".format(t[0], t[1])
-        info["geopolygon"] = s[:-1] + ")"
+        # s = "("
+        # for t in info["geopolygon"]:
+        #     s += "({},{}),".format(t[0], t[1])
+        # info["geopolygon"] = s[:-1] + ")"
         with Connection() as connect:
             self.check_info(connect)
             cur = connect.cursor()
-            cur.execute(self.sql_insert_fire_into_history, info)
             cur.execute(self.sql_insert_fire_into_info, info)
             connect.commit()
             cur.execute(self.sql_count_records)
             self.inserted_count = cur.fetchone()[0]
             cur.close()
         print("Finished inserting file:",info["firename"],info["datetime"])
+        print("record count:",self.inserted_count)
+        return info["datetime"].year, info["firename"]
+
+    def insert_history(self,year,name):
+        with Connection() as connect:
+            info = {"year":year,"name":name}
+            cur = connect.cursor()
+            cur.execute(self.sql_insert_fire_into_history, info)
+            connect.commit()
+            cur.close
 

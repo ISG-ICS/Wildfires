17d61519d2623cc893cfdfba7c27bd4c2d9f79f3
kathyzhuang <kathyzhuang378@gmail.com>
2019-08-05 15:19:23 -0700

    added search service

---
+++ frontend/src/app/services/search/search.service.ts
@@ -0,0 +1,20 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SearchService {
+
+    searchDataLoaded = new EventEmitter();
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    getSearch(userInput): Observable<any> {
+        return this.http.get("http://127.0.0.1:5000/search", {params: new HttpParams().set('keyword', userInput)})
+    }
+
+}

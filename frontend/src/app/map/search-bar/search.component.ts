de9bf59cac5174b40973527788c2c4b5035d07a3
kathyzhuang <kathyzhuang378@gmail.com>
2019-08-06 14:51:41 -0700

    modified the user input search feature

--- frontend/src/app/map/search-bar/search.component.ts
+++ frontend/src/app/map/search-bar/search.component.ts
@@ -30,51 +30,44 @@ export class SearchComponent implements OnInit {
     }
 
     dropDownHandler = (event) => {
-        // clears any possible existing value from search box
 
+        console.log('event', event);
+        // clears any possible existing value from search box
         if (event.key !== 'ArrowDown' && event.key !== 'ArrowUp' && event.key !== 'Enter') {
-            if (event.target.value !== "") {
+            if (event.target.value !== '') {
                 this.mapService.getDropBox(event.target.value).subscribe(this.getSearchInputDataHandler);
                 // gets auto-completion suggestion from the database
             }
-        } else if (event.key === 'Enter') {
-            this.selected(null, event.target.value);
-            console.log('here in enter directly')
         }
-
+        if (event.key === 'Enter') {
+            this.selected(null, event.target.value);
+            console.log('here in enter directly');
+        }
     };
+
 
     getSearchInputDataHandler = (data) => {
         // process the data, make the display look more aesthetic
         let i;
-        let cityString, countyString, stateString, city, county, state;
-        cityString = countyString = stateString = city = county = state = "";
+        let cityString, countyString, stateString;
+        cityString = countyString = stateString = '';
         let arr = {};
         this.dataToDropDownMenu = [];
 
         for (i = 0; i < data.length; i++) {
             // if the level data exists, add to the dictionary arr, then add arr to list dataToDropDownMen
             if (data[i][0]) {
-                cityString = data[i][0] + " (city) ";
-                city = data[i][0]
+                cityString = data[i][0] + ', ';
             }
             if (data[i][1]) {
-                countyString = data[i][1] + " (county) ";
-                county = data[i][1];
+                countyString = data[i][1] + ', ';
             }
             if (data[i][2]) {
-                stateString = data[i][2] + " (state)";
-                state = data[i][2];
+                stateString = data[i][2];
             }
             arr['display'] = cityString + countyString + stateString;
-            // 'display' is for showing on the drop down menu
-            if (city) {
-                arr['value'] = city;
-            } else if (county) {
-                arr['value'] = county;
-            } else {
-                arr['value'] = state;
-            }
+
+            arr['value'] = cityString + countyString + stateString;
             // 'value' is for showing on the search box
 
             arr['id'] = data[i][3];
@@ -85,7 +78,7 @@ export class SearchComponent implements OnInit {
     };
 
     userInputCheckHandler = ([[data], value]) => {
-        console.log(data);
+        console.log('data here is it null', data);
         // given the boundary data after the keyword search, fits the map according to the boundary and shows the name label
         if (data) {
             console.log('data', data);
@@ -93,7 +86,7 @@ export class SearchComponent implements OnInit {
             console.log('here, no data');
             (document.getElementById('search-input-id') as HTMLInputElement).value = '';
 
-            (document.getElementById('placeholder') as HTMLInputElement).innerHTML = 'Invalid input, please enter again';
+            (document.getElementById('placeholder') as HTMLInputElement).innerHTML = 'Please enter again';
 
         }
     };
@@ -103,20 +96,17 @@ export class SearchComponent implements OnInit {
         // FIXME: county id are all set to 6 due to the lack of county id data
         // passes the id and location name to search component
         if (id) {
-            console.log('here in selected with id')
+            console.log('here in selected with id');
             this.searchService.getSearch(id).subscribe((data) => {
-                this.searchService.searchDataLoaded.emit([data, value])
-            })
+                this.searchService.searchDataLoaded.emit([data, value]);
+            });
         } else {
-            console.log('here in selected with value');
-            console.log('id', id);
-            console.log('value', value);
 
             this.searchService.getSearch(value).subscribe((data) => {
-                this.searchService.searchDataLoaded.emit([data, value])
-            })
+                this.searchService.searchDataLoaded.emit([data, value]);
+            });
         }
 
-    }
+    };
 
 }

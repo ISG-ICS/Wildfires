d1476febb876b8befaa45217c1c657bcc2ee986e
Yicong-Huang <hyc541978023@gmail.com>
2019-08-08 17:31:03 -0700

    fixed the logger generator subscription issue

--- backend/data_preparation/dumper/url_dumper.py
+++ backend/data_preparation/dumper/url_dumper.py
@@ -13,8 +13,7 @@ logger = logging.getLogger('TaskManager')
 class URLDumper(DumperBase):
     def insert(self, data: Union[List, Dict]):
         if isinstance(data, dict):
-            Connection().sql_execute_values("insert into images(id,image_url) values", self._gen_id_url_pair(data))
-            logger.info('image: ' + self._gen_id_url_pair(data)[1] + 'inserted\n')
+            Connection().sql_execute_values("insert into images(id,image_url) values ", self._gen_id_url_pair(data))
 
     @staticmethod
     def _gen_id_url_pair(data) -> Generator[Tuple[int, str], None, None]:

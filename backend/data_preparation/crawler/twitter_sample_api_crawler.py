a200a8fdfaf9d353a75e4939687936408f6c5211
Roseart <quanzhen.du@gmail.com>
2019-11-13 11:16:42 -0800

    Update error handling part

--- backend/data_preparation/crawler/twitter_sample_api_crawler.py
+++ backend/data_preparation/crawler/twitter_sample_api_crawler.py
@@ -16,11 +16,11 @@ logger = logging.getLogger('TaskManager')
 
 
 class TweetSampleAPICrawler(CrawlerBase):
-    wait_time = 1
     MAX_WAIT_TIME = 64
 
     def __init__(self):
         super().__init__()
+        self.wait_time = 1
         self.api = twitter.Api(**parse(TWITTER_API_CONFIG_PATH, 'twitter-API'))
         self.data = []
         self.keywords = []
@@ -29,29 +29,31 @@ class TweetSampleAPICrawler(CrawlerBase):
 
     def crawl(self, keywords: List, batch_number: int = 100) -> Union[Dict, List]:
         """crawl the tweets and save them into self.data"""
-        logger.info(f'Crawler Started')
-        try:
-            for tweet in self.api.GetStreamFilter(track=keywords):
-                self.crawled_tweet_set.add(twitter.Status.NewFromJsonDict(tweet))
-                self.wait_time = 1  # reset the wait time to 1
-                if len(self.crawled_tweet_set) > batch_number:
-                    break
-        except:
-            # in this case the collected twitter id will be recorded and tried again next time
-            logger.error(f'Error: {traceback.format_exc()}')
-            slice_num = len(self.crawled_tweet_set)
+        while len(self.crawled_tweet_set) < batch_number:
+            logger.info(f'Crawler Started')
+            try:
+                for tweet in self.api.GetStreamFilter(track=keywords):
+                    self.crawled_tweet_set.add(twitter.Status.NewFromJsonDict(tweet))
+                    self.wait_time = 1  # reset the wait time to 1
+                    if len(self.crawled_tweet_set) > batch_number:
+                        break
+            except:
+                # in this case the collected twitter id will be recorded and tried again next time
+                logger.error(f'Error: {traceback.format_exc()}')
+                slice_num = len(self.crawled_tweet_set)
 
-            time.sleep(self.wait_time)
-            if self.wait_time < self.MAX_WAIT_TIME:  # set a wait time limit no longer than 64s
-                self.wait_time *= 2  # an exponential back-off pattern in subsequent reconnection attempts
-        else:
-            slice_num = batch_number
+                time.sleep(self.wait_time)
+                print(self.wait_time)
+                if self.wait_time < self.MAX_WAIT_TIME:  # set a wait time limit no longer than 64s
+                    self.wait_time *= 2  # an exponential back-off pattern in subsequent reconnection attempts
+            else:
+                slice_num = batch_number
 
-        self.data = [self.crawled_tweet_set.pop() for _ in range(slice_num)]
-        logger.info(f'Outputting {slice_num} tweets')
-        self.total_crawled_count += len(self.data)
-        logger.info(f'Total crawled count {self.total_crawled_count}')
-        return self.data
+            self.data = [self.crawled_tweet_set.pop() for _ in range(slice_num)]
+            logger.info(f'Outputting {slice_num} tweets')
+            self.total_crawled_count += len(self.data)
+            logger.info(f'Total crawled count {self.total_crawled_count}')
+            return self.data
 
 
 if __name__ == '__main__':
@@ -59,6 +61,6 @@ if __name__ == '__main__':
     logger.addHandler(logging.StreamHandler())
 
     tweet_sample_api_crawler = TweetSampleAPICrawler()
-    for _ in range(10):
-        raw_tweets = tweet_sample_api_crawler.crawl(['wildfire'], batch_number=1)
+    for _ in range(20):
+        raw_tweets = tweet_sample_api_crawler.crawl(['wildfire'], batch_number=5)
         print(raw_tweets)

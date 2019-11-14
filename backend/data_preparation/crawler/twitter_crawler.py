d991f58f5bd0dba10d5d0548d0e3707fa83b3036
Roseart <quanzhen.du@gmail.com>
2019-11-13 17:09:00 -0800

    Update error handling part and add an id_mode

--- backend/data_preparation/crawler/twitter_crawler.py
+++ backend/data_preparation/crawler/twitter_crawler.py
@@ -20,9 +20,11 @@ logger = logging.getLogger('TaskManager')
 
 
 class TweetCrawler(CrawlerBase):
+    MAX_WAIT_TIME = 64
 
     def __init__(self):
         super().__init__()
+        self.wait_time = 1
         self.api = twitter.Api(**parse(TWITTER_API_CONFIG_PATH, 'twitter-API'))
         self.data = []
         self.keywords = []
@@ -30,45 +32,55 @@ class TweetCrawler(CrawlerBase):
         self.crawled_id_set: Set[int] = set()
         self.data_from_db_count = 0
 
-    def crawl(self, keywords: List, batch_number: int, fetch_from_db: bool) -> Union[Dict, List]:
+    def crawl(self, keywords: List, batch_number: int, id_mode=True) -> Union[Dict, List]:
         """crawl the tweets and save them into self.data"""
         self.keywords = keywords
-        if not fetch_from_db:
-            # crawl status ids
-            logger.info(f"Online Mode: Total crawled count {self.total_crawled_count}")
+        if id_mode:
+            # crawl tweet ids
+            # logger.info(f"Online Mode: Total crawled count {self.total_crawled_count}")
             self.crawled_id_set = self._crawl_tweet_ids()
-            logger.info(f"Online Mode: crawled {len(self.crawled_id_set)}")
+            # logger.info(f"Online Mode: crawled {len(self.crawled_id_set)}")
 
+            # gets status with the list that has batch number (can be a bit more than the batch#) amount of tweets
             while len(self.crawled_id_set) < batch_number:
                 # loops until the number of id collected is greater than the batch number
                 current_count = len(self.crawled_id_set)
-                time.sleep(10)
+                time.sleep(0.1)
                 self.crawled_id_set.update(self._crawl_tweet_ids())
                 if len(self.crawled_id_set) > current_count:
-                    logger.info(f"Online Mode: crawled: {len(self.crawled_id_set)}")
+                    logger.info(f"ID Mode: crawled: {len(self.crawled_id_set)}")
 
-            # gets status with the list that has batch number (can be a bit more than the batch#) amount of tweets
-            ids = list(self.crawled_id_set)
+            # turn ids into list of dict and turn each dict into Status
+            id_dict = [{'id': int(id)} for id in self.crawled_id_set]
+            status_id = [twitter.Status.NewFromJsonDict(id_dict_item) for id_dict_item in id_dict]
+            self.crawled_id_set.clear()
+
+            return status_id
         else:
             # reprocess the crawled status ids that are stored in db
             ids = next(self.fetch_status_id_from_db())
             self.total_crawled_count += len(ids)
             logger.info(f'DB Mode: ids taken from db length {len(ids)}')
             logger.info(f'DB Mode: Total crawled count {self.total_crawled_count}')
-        try:
-            self.data = self.api.GetStatuses(ids)
+            while not self.data:
+                try:
+                    self.data = self.api.GetStatuses(ids)
 
-            # reset the set to empty so that the id will not accumulate
-            # in the case that the twitter API works
-        except Exception:
-            logger.error('error: ' + traceback.format_exc())
-            # in this case the collected twitter id will be recorded and tried again next time
-        else:
-            self.crawled_id_set.clear()
-        # save crawled to self.data (in-memory), or, if needed, to disk file
-        # also return a reference of self.data
+                    # reset the set to empty so that the id will not accumulate
+                    # in the case that the twitter API works
+                except:
+                    logger.error('error: ' + traceback.format_exc())
+                    # in this case the collected twitter id will be recorded and tried again next time
 
-        return self.data
+                    time.sleep(self.wait_time)
+                    if self.wait_time < self.MAX_WAIT_TIME:  # set a wait time limit no longer than 64s
+                        self.wait_time *= 2  # an exponential back-off pattern in subsequent reconnection attempts
+                else:
+                    self.crawled_id_set.clear()
+                # save crawled to self.data (in-memory), or, if needed, to disk file
+                # also return a reference of self.data
+
+                return self.data
 
     def _crawl_tweet_ids(self) -> Set[int]:
         """helper function for crawling tweets, collecting all tweet ids"""
@@ -115,10 +127,10 @@ if __name__ == '__main__':
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
 
-    # online mode
-    raw_tweets = TweetCrawler().crawl(['wildfires'], batch_number=100, fetch_from_db=False)
+    # id_mode
+    raw_tweets = TweetCrawler().crawl(['wildfire'], batch_number=100)
     print(raw_tweets)
 
     # fetch_from_db mode
-    raw_tweets = TweetCrawler().crawl(['wildfires'], batch_number=100, fetch_from_db=True)
+    raw_tweets = TweetCrawler().crawl(['wildfire'], batch_number=100, id_mode=False)
     print(raw_tweets)

563ebdc32c16eb41b006dd8bbeb89df44da84827
Roseart <quanzhen.du@gmail.com>
2019-11-22 16:00:50 -0800

    add log for keywords set

--- backend/task/text_from_twitter_runnable.py
+++ backend/task/text_from_twitter_runnable.py
@@ -51,10 +51,11 @@ class TextFromTwitter(Runnable):
             self.crawler = TweetFilterAPICrawler()
 
         while True:
-            # add fires that happened in the past month and put these fires into the keywords for search 
+            # add fires that happened in the past month and put these fires into the keywords for search
             fire_names = {fire_name.lower() for fire_name, in Connection.sql_execute(
                 f"select name from fire_merged where start_time > now() - interval '1 month'")}
             keywords_with_fire_names = fire_names | (set(keywords))
+            logger.info(f'Full keyword set: {keywords_with_fire_names}')
             logger.info('Running ID mode')
             ids = self.crawler.crawl(list(keywords_with_fire_names), batch_num)
             self.dumper.insert(ids, id_mode=True)
